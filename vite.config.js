d0058048ad5d321dd9675e1d73589c06cc53220a
karishmanaula <karishma786naula@gmail.com>
2025-06-29 18:45:39 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/02counter/',
-  lugins: [react()],
+   base: '/02counter/',
+  plugins: [react()],
 })
 
